03ee0f1713608760a73fea64a29e53e99a510e10
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2021-07-13 19:22:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Basic setup
+termux-setup-storage
+sleep 5
+
 #Clean Install
 if [[ ! -d bin ]]; then
   mkdir bin
@@ -8,13 +13,13 @@ if [[ -e bin\\termux-url-opener ]]; then
 fi 
 
 #Installing dependencies
-#pkg update -y && pkg install python -y && pkg install ffmpeg -y && pip install youtube-dl -y
+pkg update -y && pkg install python -y && pkg install ffmpeg -y && pip install youtube-dl -y
 
 value=$(< termux-url-opener)
 
 # more termux-url-opener > "./bin/termux-url-opener"
-echo "$value" > "./bin/termux-url-opener"
+curl -L https://raw.githubusercontent.com/LZNOXP/termux-ytdl/main/termux-url-opener > "./bin/termux-url-opener"
 chmod +x ./bin/termux-url-opener
-
+curl -L https://raw.githubusercontent.com/LZNOXP/termux-ytdl/main/simple-ytdl.py > "simple-ytdl.py"
 read -p "Where should the downloaded video be saved? : " path
 python3 ./simple-ytdl.py -p "$path"
