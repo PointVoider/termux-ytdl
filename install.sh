0e80086d514e96758cf2f44203d5f1005449def8
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2021-07-13 19:30:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,11 +13,8 @@ if [[ -e ~/bin/termux-url-opener ]]; then
 fi 
 
 #Installing dependencies
-pkg update -y && pkg install python -y && pkg install ffmpeg -y && pip install youtube-dl -y
+#pkg update -y && pkg install python -y && pkg install ffmpeg -y && pip install youtube-dl
 
-value=$(< termux-url-opener)
-
-# more termux-url-opener > "./bin/termux-url-opener"
 curl -L https://raw.githubusercontent.com/LZNOXP/termux-ytdl/main/termux-url-opener > "./bin/termux-url-opener"
 chmod +x ./bin/termux-url-opener
 curl -L https://raw.githubusercontent.com/LZNOXP/termux-ytdl/main/simple-ytdl.py > "simple-ytdl.py"
