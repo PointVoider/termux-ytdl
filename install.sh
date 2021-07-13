472ec41432b42c468398a51fc3234b7156225243
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2021-07-13 12:31:38 +0000

    Create install.sh

--- install.sh
+++ install.sh
@@ -12,17 +12,16 @@ if [[ -e ~/bin/termux-url-opener ]]; then
   rm ~/bin/termux-url-opener
 fi 
 
+read -p "Where should the downloaded video be saved? : " path
+
 curl -L https://raw.githubusercontent.com/LZNOXP/termux-ytdl/main/termux-url-opener > "./bin/termux-url-opener"
 chmod +x ./bin/termux-url-opener
 curl -L https://raw.githubusercontent.com/LZNOXP/termux-ytdl/main/simple-ytdl.py > "simple-ytdl.py"
 
 
 # Basic setup
-termux-setup-storage
-sleep 2
 apt-get update
 apt-get -y install python ffmpeg
 pip install youtube-dl
 
-read -p "Where should the downloaded video be saved? : " path
 python3 ~/simple-ytdl.py -p "$path"
